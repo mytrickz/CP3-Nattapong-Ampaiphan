910ec095087eb2b826083100a9c06519d882655b
mytrickz <nattapongnex@users.noreply.github.com>
2019-05-15 20:19:31 +0700

    Exercise4_Python_BorntoDev

---
+++ Exercise4_Nattapong_A.py
@@ -0,0 +1,10 @@
+EngScore = 100.0
+BusinessScore = 100.0
+ComSysScore = 100.0
+ComProScore = 100.0
+
+print("--- Your Score ---")
+print("Foundation English :", EngScore)
+print("General Business :", BusinessScore)
+print("Introduction to Computer Systems :", ComSysScore)
+print("Computer Programming :", ComProScore)
