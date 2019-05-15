4ba159c5a92e6aa64635c374bf1277ffce40dbbc
mytrickz <nattapongnex@users.noreply.github.com>
2019-05-15 22:53:40 +0700

    Exercise5_1

---
+++ Exercise5_1_Nattapong_A.py
@@ -0,0 +1,6 @@
+val1 = int(input("1st number : "))
+val2 = int(input("2nd number : "))
+print(val1, "+", val2, "=", val1+val2)
+print(val1, "-", val2, "=", val1-val2)
+print(val1, "*", val2, "=", val1*val2)
+print(val1, "/", val2, "=", int(val1/val2))
