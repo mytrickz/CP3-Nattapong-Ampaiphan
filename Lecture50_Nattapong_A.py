b025f5c0f14ba72810ae5c61b6e2278b1fe80f7d
mytrickz <nattapongnex@users.noreply.github.com>
2019-05-19 10:10:18 +0700

    Lecture50

---
+++ Lecture50_Nattapong_A.py
@@ -0,0 +1,11 @@
+def plusNumber(x,y):
+    print(x,"+",y,"=",x+y)
+
+def minusNumber(x,y):
+    print(x,"-",y,"=",x-y)
+
+def mutiplyNumber(x,y):
+    print(x,"x",y,"=",x*y)
+
+def divideNumber(x,y):
+    print(x,"/",y,"=",x/y)
